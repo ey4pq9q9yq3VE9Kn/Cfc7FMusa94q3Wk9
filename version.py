d7485fb25f753a8de61c430fe93f61ce12324473
ey4pq9q9yq3VE9Kn <82398961+ey4pq9q9yq3VE9Kn@users.noreply.github.com>
2021-04-12 17:56:33 +0200

    Create version.py

---
+++ version.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__version__ = "2021.04.12"
+__title__ = 'Cfc7FMusa94q3Wk9'
+__author__ = 'ey4pq9q9yq3VE9Kn'
+__license__ = 'GPLv3+'
+__copyright__ = 'Copyright 2021 ey4pq9q9yq3VE9Kn'
+
+
+def get_version() -> str:
+    """Get and return foo's version."""
+    return __version__
